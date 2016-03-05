9e187c04c4380c207c45d3b0d9e5de7c6e5fad12
zaid09 <analchoudhury1@gmail.com>
2016-03-05 11:12:04 +0530

    JavaScript file

---
+++ reaction tester/script.js
@@ -0,0 +1,37 @@
+function getRandomColor() {
+    		var letters = '0123456789ABCDEF'.split('');
+    		var color = '#';
+    		for (var i = 0; i < 6; i++ ) {
+        		color += letters[Math.floor(Math.random() * 16)];
+    		}
+    		return color;
+	}
+	var clickedTime; var createdTime; var reactionTime;
+	function makebox() {
+		var time=Math.random();
+		time=time*5000;
+		setTimeout(function() {
+		if(Math.random()>0.5) {
+			document.getElementById("box").style.borderRadius="100px";
+		} else {
+			document.getElementById("box").style.borderRadius="0";
+		}
+		var top =Math.random();
+		top=top*300;
+		var left =Math.random();
+		left=left*300;
+		document.getElementById("box").style.top=top+"px";
+		document.getElementById("box").style.left=left+"px";
+		document.getElementById("box").style.backgroundColor=getRandomColor();
+		document.getElementById("box").style.display="block";
+		createdTime=Date.now();
+		},time);
+	}
+	document.getElementById("box").onclick=function() {
+		clickedTime=Date.now();
+		reactionTime=(clickedTime-createdTime)/1000;
+		document.getElementById("time").innerHTML=reactionTime;
+		this.style.display="none";
+		makebox();
+	}
+	makebox();
